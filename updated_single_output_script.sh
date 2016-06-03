da9a1fda4abce1092ddb3d2ee4276206a39b2143
houghb <houghb@uw.edu>
2016-06-03 08:24:41 -0700

    finished gridsearches, training final full network, and retraining individual networks

---
+++ updated_single_output_script.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+python create_and_train_single-output_updated.py 0 7 1.5
+wait
+python create_and_train_single-output_updated.py 1 16 0.8
+wait
+python create_and_train_single-output_updated.py 2 21 0.2
+wait
+python create_and_train_single-output_updated.py 3 6 1.5
+wait
+python create_and_train_single-output_updated.py 4 12 1.5
+wait
+python create_and_train_single-output_updated.py 5 29 1.5
+wait
+python create_and_train_single-output_updated.py 6 11 0.7
+wait
+python create_and_train_single-output_updated.py 7 21 1.5
+wait
+python create_and_train_single-output_updated.py 8 16 0.1
+wait
+python create_and_train_single-output_updated.py 9 28 0.1
+wait
+python create_and_train_single-output_updated.py 10 11 0.1
+wait
+python create_and_train_single-output_updated.py 11 18 0.1
+wait
+python create_and_train_single-output_updated.py 12 19 0.1
+wait
+python create_and_train_single-output_updated.py 13 28 0.2
+wait
+python create_and_train_single-output_updated.py 14 14 1.5
+wait
+python create_and_train_single-output_updated.py 15 17 1.5
+wait
+python create_and_train_single-output_updated.py 16 5 0.8
+wait
+python create_and_train_single-output_updated.py 17 25 1.5
+wait
+python create_and_train_single-output_updated.py 18 17 1.5
+wait
+python create_and_train_single-output_updated.py 19 18 0.1
+wait
+python create_and_train_single-output_updated.py 20 27 0.8
+wait
+python create_and_train_single-output_updated.py 21 14 1.5
+wait
+python create_and_train_single-output_updated.py 22 7 1.5
+wait
+python create_and_train_single-output_updated.py 23 16 1.4
+wait
+python create_and_train_single-output_updated.py 24 17 0.1
+wait
+python create_and_train_single-output_updated.py 25 18 0.2
+wait
+python create_and_train_single-output_updated.py 26 11 1.4
+wait
+python create_and_train_single-output_updated.py 27 11 1.4
+wait
+python create_and_train_single-output_updated.py 28 17 1.4
+wait
+python create_and_train_single-output_updated.py 29 32 0.9
