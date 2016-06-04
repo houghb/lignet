822a92c2bed8c218450f427430d4b1b9562975e6
houghb <houghb@uw.edu>
2016-06-04 12:44:37 -0700

    completed decision tree gridsearch

--- gridsearches/gridsearch_decision_tree.py
+++ gridsearches/gridsearch_decision_tree.py
@@ -20,7 +20,7 @@ x_train, x_test, y_train, y_test, x_scaler, y_scaler = gen_train_test()
 dtr_full = tree.DecisionTreeRegressor(max_depth=20, min_samples_leaf=5)
 
 # Set up the gridsearch
-param_grid = {'max_depth': range(10, 25),
+param_grid = {'max_depth': range(10, 40),
               'min_samples_leaf': range(1, 15),
               }
 
@@ -29,11 +29,11 @@ grid_search = GridSearchCV(dtr_full, param_grid, verbose=0, n_jobs=20,
                            scoring='mean_squared_error')
 
 # search the network parameters using a subset of the entire training set
-grid_search.fit(x_train[:50000, :], y_train[:50000, :])
+grid_search.fit(x_train[:, :], y_train[:, :])
 
 script_running_time = time.time() - script_start_time
 
-with open('decision_tree_gridsearch_report.txt', 'a') as report:
+with open('decision_tree_gridsearch_report.txt', 'w+') as report:
     print >>report, ('took % sec\n\n' % script_running_time)
     for entry in grid_search.grid_scores_:
        print >>report, entry
