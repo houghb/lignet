97ed3efd9a653acce772dd3934fa89d3f6203b7d
houghb <houghb@uw.edu>
2016-06-05 14:00:01 -0700

    determined optimal architechture for decision tree

---
+++ lignet/create_and_train_decision_tree.py
@@ -0,0 +1,17 @@
+"""
+Train the decision tree that we've determined gives the best performance on
+our training data.  See the decision_tree ipython notebook for more details.
+"""
+
+import cPickle as pickle
+from sklearn import tree
+from lignet_utils import gen_train_test
+
+x_train, x_test, y_train, y_test, x_scaler, y_scaler = gen_train_test()
+
+dtr_full = tree.DecisionTreeRegressor(max_depth=26, min_samples_leaf=2)
+dtr_full = dtr_full.fit(x_train, y_train)
+
+# pickle the trained regressor
+with open('trained_networks/decision_tree.pkl', 'wb') as pkl:
+    pickle.dump([dtr_full], pkl)
